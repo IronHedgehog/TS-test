dbf9a86a4e5c24535fe1b51709880138e3d73037
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-10-02 16:07:47 +0300

    less2

--- less2.ts
+++ less2.ts
@@ -20,3 +20,68 @@ if (typeof some === "string") {
   str = some;
 }
 // --------------------------------------------------------------------
+
+let person1: [string, number];
+person1 = ["text", 1];
+// --------------------------------------------------------------------
+
+enum loadingStatus {
+  LOADING,
+  READY,
+}
+
+const person2 = {
+  status: loadingStatus.READY,
+};
+
+if (person2.status === loadingStatus.READY) {
+  console.log("Role: ", loadingStatus.READY);
+}
+
+// --------------------------------------------------------------------
+
+const a: string | number = 5;
+// --------------------------------------------------------------------
+let literal: "enable" | "disable";
+literal = "disable";
+// --------------------------------------------------------------------
+function showMessage(message): void {
+  console.log(message);
+}
+
+function calc(num1, num2): number {
+  return num1 + num2;
+}
+
+function customError(): never {
+  throw new Error("Error");
+}
+// --------------------------------------------------------------------
+type pageData = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: string;
+    updateAt: string;
+  };
+};
+
+const page1: pageData = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: "2021-01-01",
+    updateAt: "2021-05-01",
+  },
+};
+
+const page2: pageData = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "close",
+};
