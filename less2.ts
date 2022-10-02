c1cb29389c40c3c687cabc41af684e91a03afe19
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-10-02 15:43:47 +0300

    training

---
+++ less2.ts
@@ -0,0 +1,22 @@
+let age: number = 50;
+let name1: string = "Max";
+let toggle: boolean = true;
+let empty: null = null;
+let notInitialize: undefined = undefined;
+let callback = (a: number): number => {
+  return 100 + a;
+};
+// --------------------------------------------------------------------
+let anything: any = -20;
+anything = "Text";
+anything = {};
+// --------------------------------------------------------------------
+let some: unknown;
+some = "Text";
+
+let str: string;
+
+if (typeof some === "string") {
+  str = some;
+}
+// --------------------------------------------------------------------
